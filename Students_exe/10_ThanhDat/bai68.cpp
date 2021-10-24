19bb758e0fcbc14bd85ca914b580be84bd372705
ldss13 <lds050606@gmail.com>
2021-10-24 17:30:17 +0700

    Create bai68.cpp

---
+++ Students_exe/10_ThanhDat/bai68.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int sum(int n,int x)
+{
+    int Sum=0;
+    for(int so_n=1;so_n<=n;so_n++)
+    {
+        Sum+=(pow(-1,so_n)*pow(x,2*so_n));
+    }
+    return Sum;
+}
+
+int main(){
+    int n;
+    int x;
+    cin>>n>>x;
+    cout<<sum(n,x);
+    return 0;
+}
