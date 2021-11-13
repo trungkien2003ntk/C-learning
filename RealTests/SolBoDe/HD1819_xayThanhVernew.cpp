85861f1c7ecfb2f8e491abbfb81c53bf2f428fb4
medtik <longpham6595@gmail.com>
2021-11-13 22:40:40 +0700

    Xây thành chuẩn hóa theo hướng các bạn

---
+++ RealTests/SolBoDe/HD1819_xayThanhVernew.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string st,subst,arr[100000],save[100000];
+int cs=0;
+
+void xuly();
+bool check();
+
+bool check(string x)
+{
+    bool kq = false;
+    for (int i = 0;i<=cs;i++)
+        if (save[i]==x)
+            kq = true;
+    return kq;
+}
+void xuly()
+{
+    getline(cin,st);
+    for (int i = 0; i<=st.length();i++)
+        for (int j=1;j<=st.length()-i;j++)
+            {
+                subst = st.substr(i,j);
+                if (!check(subst))
+                    {
+                        cs++;
+                        save[cs] = subst;
+                    }
+            }
+    cout << cs << endl;
+}
+
+int main()
+{
+    xuly();
+    return 0;
+}
