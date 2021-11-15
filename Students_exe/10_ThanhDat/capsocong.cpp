3872b6d349f5bcf0a046d42e129604569e1f387e
lethanhdat <91755733+ldss13@users.noreply.github.com>
2021-11-16 00:15:19 +0700

    Update capsocong.cpp

--- Students_exe/10_ThanhDat/capsocong.cpp
+++ Students_exe/10_ThanhDat/capsocong.cpp
@@ -32,28 +32,28 @@ bool kotrung(int n)
 void xuly()
 {
     
-    for(int x=0;x<n;x++)
+    for(int x=0;x<n;x++) //chay tu so dau den so cuoi
     {   
-        if(kotrung(arr[x]))
+        if(kotrung(arr[x])) //số đó đã có trong arr check hay chưa truong hop 1 3 7 thì xet 1 r khong xet 3 nữa vì 1 đã có 1 3 7 thì kiểu gì thằng 3 cũng bé hơn dãy 1
         {   
-            c=arr[x];
-            dem=1;
-            arr_check[carr_check]=arr[x];
-            carr_check++;
-            for(int i=0;i<n;i++)
+            c=arr[x]; // c se bang so do
+            dem=1;// tinh luon so do la 1
+            arr_check[carr_check]=arr[x]; // dua con so dau tiên vào arr check
+            carr_check++; // cộng cound của arr lên để lưu cho arr tiếp theo
+            for(int i=0;i<n;i++) // chay tu so dau den so cuoi
             {
-                if(arr[i]==c+d && kotrung(arr[i]))
+                if(arr[i]==c+d && kotrung(arr[i])) // neu co so nao = số đó + d  và kiem tra xem số đó đã có trong arr check hay chưa 
                 {
-                    arr_check[carr_check]=arr[i];
-                    carr_check++;
-                    dem++;
-                    c=c+d;
-                    i=0;
+                    arr_check[carr_check]=arr[i]; // đưa số đó vào arr check
+                    carr_check++; // côngj count arr
+                    dem++; //cong đếm
+                    c=c+d; // c lúc này sẽ là c + d
+                    i=0; // reset i=0 để xét từ đầu dãy trường hợp 1 7 3 thì số 7 nằm trước số 3 nếu ko reset sẽ ko tìm được
                 }
             }
-            arr_ss[carr_ss]=dem;
-            carr_ss++;
-        }     
+            arr_ss[carr_ss]=dem; // sau khi xet xong sẽ có 1 thằng đếm là số lần mà nó thỏa cái if , bê đếm thứ nhất vào 1 arr so sánh
+            carr_ss++; // cộng lên để cho lần lưu tiep theo
+        }     // rồi quay lại xét số thứ 2 
     }
     for(int x=0;x<carr_ss;x++)
         cout<<arr_ss[x]<<" ";
@@ -62,7 +62,7 @@ void xuly()
         cout<<arr_check[x]<<" ";
 }
 
-int max()
+int max() // tìm max trong dãy đếm
 {
     int max=arr_ss[0];
     for(int x=1;x<carr_ss;x++)
