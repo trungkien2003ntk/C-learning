b1578c2acba0fc0f2cf08aa2a06c6474344fd4e9
Nguyen Kien <trungkien2003ntk@gmail.com>
2021-12-16 19:55:45 +0700

    Update bai1_timgiua.cpp

--- RealTests/HaNoi2020/Sols/LoiGiaiToantap/LoiGiaiToantap/bai1_timgiua.cpp
+++ RealTests/HaNoi2020/Sols/LoiGiaiToantap/LoiGiaiToantap/bai1_timgiua.cpp
@@ -2,14 +2,14 @@
 #include <fstream>
 using namespace std;
 
-unsigned long long l, r;
+ long long l, r;
 
 void nhap();
-unsigned long long sumlr(unsigned long long x, unsigned long long y);
+ long long sumlr( long long x,  long long y);
 void xuly();
-int choose(unsigned long long x);
-void xuat(unsigned long long x);
-unsigned long long tuyetdoi(unsigned long long x);
+int choose( long long x);
+void xuat( long long x);
+long long tuyetdoi( long long x);
 // Chuong trinh nhap
 void nhap() {
 	ifstream f;
@@ -19,18 +19,18 @@ void nhap() {
 }
 
 // Tinh tong cac so tu x den y 
-unsigned long long sumlr(unsigned long long x, unsigned long long y) {
+long long sumlr(long long x,  long long y) {
 	return ((x + y)*(y - x + 1) / 2);
 }
 
-unsigned long long tuyetdoi(unsigned long long x){
+long long tuyetdoi( long long x){
 	if (x>0)
 		return x;
 	return -x;
 }
 
 // Chon chi so xuat ra 
-int choose(unsigned long long x){
+int choose( long long x){
 	if (tuyetdoi(sumlr(l,x-1)-sumlr(x,r))>tuyetdoi(sumlr(l,x)-sumlr(x+1,r)))
 		return x;
 	return x-1;
@@ -47,8 +47,8 @@ void xuat(int x){
 // Chuong trinh xu ly
 void xuly() {
 	// Luu midsum = 1/2 tong tu l den r
-	unsigned long long midsum = sumlr(l, r)/2;
-	unsigned long long cs = r;
+	 long long midsum = sumlr(l, r)/2;
+	 long long cs = r;
 	while (sumlr(cs, r) < midsum)
 		cs--;
 	xuat(choose(cs));
