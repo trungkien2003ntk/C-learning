f7259219ab2b3f3c0ecf67bd94dc78c192efc3d7
medtik <longpham6595@gmail.com>
2022-01-05 01:42:26 +0700

    Bài 3 Ngày 2 LA21-22

---
+++ RealTests/LongAn2021-2022/LG/b3n2_wifi.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+long long M,N,K,maxB,countMaxB,coor_x[1000],coor_y[1000],range_R[1000],bandwidth_B[1000],MAX_X,MAX_Y,MAX_R;
+void Nhap(){
+	ifstream f;
+	f.open("wifi.inp");
+	f >> M >> N >> K;
+	for (int i=1;i<=K;i++)
+		f >> coor_x[i] >> coor_y[i] >> range_R[i] >> bandwidth_B[i];
+	f.close();
+}
+
+void KhoiTao(){
+	MAX_X = coor_x[1];
+	MAX_Y = coor_y[1];
+	MAX_R = range_R[1];
+	for (int i=2;i<=M;i++){
+		if (MAX_X < coor_x[i])
+			MAX_X = coor_x[i];
+		if (MAX_Y < coor_y[i])
+			MAX_Y = coor_y[i];
+		if (MAX_R < range_R[i])
+			MAX_R = range_R[i];
+	}	
+	MAX_X += MAX_R;
+	MAX_Y += MAX_R;
+}
+
+float Distance(long long x1,long long y1,long long x2, long long y2){
+	return (sqrt(pow((x1-x2),2)+pow((y1-y2),2)));
+}
+
+long long Speed(long long x,long long y){
+	long long kq=0;
+	for (int i=1;i<=K;i++)
+		if (Distance(x,y,coor_x[i],coor_y[i])<=range_R[i])
+			kq += bandwidth_B[i];
+	return kq;
+}
+
+void XuLy(){
+	maxB = countMaxB=0;
+	for (int i=1;i<=MAX_X;i++)
+		for (int j=1;j<=MAX_Y;j++){
+			if ((maxB == 0)||(maxB<Speed(i,j))){
+				maxB = Speed(i,j);	
+				countMaxB = 1;
+			}
+			else if (maxB == Speed(i,j)){
+				countMaxB++;
+//				cout << "(i,j)=(" << i << "," << j << ")" << endl; 
+			}
+		}
+//	cout << maxB << " " << countMaxB;
+}
+
+void Xuat(){
+	ofstream f;
+	f.open("wifi.out");
+	f << maxB << endl;
+	f << countMaxB;
+	f.close();	
+}
+
+
+int main(){
+	Nhap();
+	KhoiTao();
+	XuLy();
+	Xuat();
+	return 0;
+}
