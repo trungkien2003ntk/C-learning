552aea19127cc302c1ba1c8030b6578356159ee8
medtik <longpham6595@gmail.com>
2024-02-21 21:19:21 +0700

    update THCS Như Bài 3
    Bài 3 2 version cho Như.

---
+++ THCS/HSG LA 2023/Huyen2023/bai3_vernormal.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+// SPEED UP
+// #pragma GCC optimize("O3") //how good is this? lol
+// #pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,fma")
+// #pragma GCC optimize("unroll-loops")
+#define fast_io                   \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+//============================================================================
+// START PROGRAM
+//============================================================================
+bool isPowerOfTwo(int x)
+{
+    int tempt = x;
+    while (tempt > 0 && tempt % 2 == 0)
+        tempt /= 2;
+    if (tempt == 1)
+        return true;
+    return false;
+}
+
+int n, it;
+
+int main()
+{
+    fast_io;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> it;
+        if (isPowerOfTwo(it - 1))
+            cout << it << " ";
+    }
+    // getchar();
+}
